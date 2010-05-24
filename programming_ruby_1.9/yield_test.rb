533f30569dd6d7acfc9a605febbe66292c2ad2eb
leoluz <leonardo.la@gmail.com>
2010-05-23 22:00:56 -0300

    adiciona alguns testes com yield

--- programming_ruby_1.9/yield_test.rb
+++ programming_ruby_1.9/yield_test.rb
@@ -8,7 +8,19 @@ def fibonacci_until(max)
     end
 end
 
-fibonacci_until(20) do |i|
+fibonacci_until(10) do |i|
     puts ">> O bloco recebe o valor de x(#{i}) e em seguida o incrementa"
     i += 1
 end
+
+computer = ["H", "A", "L"].collect { |x| x.succ }
+puts "Usando o metodo collect para criar um novo array: #{computer}"
+
+puts "---"
+
+total = [1,2,3,4,5].inject(0) { |soma, elemento| soma + elemento }
+puts "Usando o metodo inject para armazenar o valor total da soma dos elementos: #{total}"
+
+total = [1,2,3,4,5].inject(1) { |produto, elemento| produto * elemento }
+puts "Usando o metodo inject para armazenar o produto de todos elementos: #{total}"
+
